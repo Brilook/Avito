43f244a959a4df1c17b3f75a775b2b156b924c88
brilook <brilook@gmail.com>
2020-07-09 18:03:35 +0300

    start add functional for form

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 'use strict';
+const dataBase = [];
 
 const modalAdd = document.querySelector('.modal__add');
 const addAd = document.querySelector('.add__ad');
@@ -6,46 +7,62 @@ const modalBtnSubmit = document.querySelector('.modal__btn-submit');
 const modalSubmit = document.querySelector('.modal__submit');
 const modalItem = document.querySelector('.modal__item');
 const catalog = document.querySelector('.catalog');
+const modalSubmitElements = [...modalSubmit.elements].filter(elem => elem.tagName !== 'BUTTON');
+const modalBtnWarning = document.querySelector('.modal__btn-warning');
+
+
+const closeModal = function (event) {
+    const target = event.target;
+    if (target.closest('.modal__close') ||
+        target === this) {
+        this.classList.add('hide');
+        if (this === modalAdd) {
+            modalSubmit.reset()
+        };
+    }
+};
+
+const closeModalEsc = event => {
+    if (event.code === 'Escape') {
+        modalAdd.classList.add('hide');
+        modalItem.classList.add('hide');
+        modalSubmit.reset();
+        document.removeEventListener('keyup', closeModalEsc);
+    }
+};
+
+modalSubmit.addEventListener('input', () => {
+    const validForm = modalSubmitElements.every(elem => elem.value);
+    modalBtnSubmit.disabled = !validForm;
+    modalBtnWarning.style.display = validForm ? 'none' : '';
+});
+
+modalSubmit.addEventListener('submit', event => {
+    event.preventDefault();
+    const itemObj = {};
+    for (const elem of modalSubmitElements) {
+        itemObj[elem.name] = elem.value;
+    }
+    dataBase.push(itemObj);
+    modalSubmit.reset();// перенести
+});
 
 addAd.addEventListener('click', () => {
     modalAdd.classList.remove('hide');
     modalBtnSubmit.disabled = true;
+    document.addEventListener('keyup', closeModalEsc);
 });
 
-modalAdd.addEventListener('click', event => {
-    const target = event.target;
 
-    if (target.classList.contains('modal__close') ||
-        target === modalAdd) {
-        modalAdd.classList.add('hide');
-        modalSubmit.reset();
-    }
-});
-
-// close the modal window by clicking on "Escape"
-document.addEventListener('keydown', event => {
-    
-    if (event.code == 'Escape');
-    modalAdd.classList.add('hide');
-    modalItem.classList.add('hide');
-    modalSubmit.reset();
-});
-
-// open modal for card
 catalog.addEventListener('click', event => {
     const target = event.target;
     const card = target.closest('.card');
-
     if (card) {
         modalItem.classList.remove('hide');
+        document.addEventListener('keyup', closeModalEsc);
     }
 });
 
-modalItem.addEventListener('click', () => {
-    const target = event.target;
 
-    if (target.classList.contains('modal__close') ||
-        target === modalItem) {
-        modalItem.classList.add('hide');
-    }
-});
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);
